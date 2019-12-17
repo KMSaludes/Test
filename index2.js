db11b9d6cd4920204cb20a19e163a60b5d5c24d0
unknown <cookeemonster28@gmail.com>
2019-12-17 19:49:21 +0800

    add new files

---
+++ index2.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(#butones).click(function(){
+        $("#cotainer").load("dialogbox.html");
+
+        });
+
+        $("#container").load("table.html");
+
+    });
